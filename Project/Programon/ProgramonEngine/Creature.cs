623187d4adac51a46cfd876bc6f3912d7035cb8f
Joell Stehouwer <joell@stehouwer.net>
2014-12-05 12:15:39 +0100

    Added programon class
    -added programon class
    -added stats struct

---
+++ Project/Programon/ProgramonEngine/Creature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace ProgramonEngine
+{
+    public class Creature
+    {
+        public byte level;
+        public int exp;
+        public Stats programonStats;
+        public Stats statsPerLevel;
+        public Sprite programonSprite;
+
+        public Creature(byte level, Stats programonStats, Stats statsPerLevel)
+        {
+            this.level = level;
+            this.programonStats = programonStats;
+            this.statsPerLevel = statsPerLevel;
+
+        }
+    }
+}
