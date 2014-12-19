98438d8ffd351e40f053219e9a6460f6e04027b3
Frank <largewowfan@gmail.com>
2014-12-19 16:30:39 +0100

    Text Sprite
    -Added text initializer to sprite (scaling not optimal).

--- Project/Programon/ProgramonEngine/Drawing/Sprite.cs
+++ Project/Programon/ProgramonEngine/Drawing/Sprite.cs
@@ -29,7 +29,6 @@ namespace ProgramonEngine
         /// <summary> Returns a Sprite with a static color. </summary>
         /// <param name="staticColor"> The color of the texture. </param>
         /// <param name="tint"> A tint for the Sprite. </param>
-        /// <param name="device"> The graphicsDevice to display the Sprite. </param>
         /// <returns> Static Color Sprite. </returns>
         public static Sprite FromStaticColor(Color staticColor, Color tint, GraphicsDevice device)
         {
@@ -48,6 +47,35 @@ namespace ProgramonEngine
             texture.SetData<Color>(colorData);
 
             return new Sprite(texture, tint);
+        }
+
+        /// <summary> Returns a Sprite with the riven text. </summary>
+        /// <param name="color"> The color of the text. </param>
+        /// <param name="scale"> The scale of the texture. </param>
+        /// <returns> Text Sprite </returns>
+        public static Sprite FromString(string text, SpriteFont font, Color color, Vector2 scale, GraphicsDevice device)
+        {
+            Vector2 textSize = font.MeasureString(text) * scale;
+            RenderTarget2D target = new RenderTarget2D(device, (int)textSize.X, (int)textSize.Y);
+            SpriteBatch sb = new SpriteBatch(device);
+
+            device.SetRenderTarget(target);
+            device.Clear(Color.Transparent);
+
+            sb.Begin();
+            sb.DrawString(font, text, Vector2.Zero, color, 0f,Vector2.Zero, scale, SpriteEffects.None, 0f);
+            sb.End();
+
+            device.SetRenderTarget(null);
+
+            Texture2D texture = new Texture2D(device, target.Width, target.Height);
+            Color[] colorData = new Color[target.Width * target.Height];
+            target.GetData<Color>(colorData);
+            texture.SetData<Color>(colorData);
+
+            target.Dispose();
+
+            return new Sprite(texture);
         }
     }
 }
