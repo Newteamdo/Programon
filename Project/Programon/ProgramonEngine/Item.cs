41617b1c044f2989406e08e2ea88634822be5873
Marco Havermans <marcohavermans@hotmail.nl>
2015-01-05 16:19:45 +0100

    Item class added descriptoion
    -Item class added descriptoion

--- Project/Programon/ProgramonEngine/Item.cs
+++ Project/Programon/ProgramonEngine/Item.cs
@@ -8,26 +8,33 @@ namespace ProgramonEngine
     public class Item
     {
         public string name;
+        public string description;
         public Sprite sprite;
         public Stats statChange;
         public Buff buffOnUse;
+
         public Item()
-        { 
-        this.name="";
-        this.sprite=new Sprite();
-        this.statChange=new Stats();
-        this.buffOnUse=new Buff();
-        }
-        public Item(string name, Sprite sprite)
         {
-            this.name = name;
-            this.sprite =sprite;
+            this.name = "";
+            this.description = "";
+            this.sprite = new Sprite();
             this.statChange = new Stats();
             this.buffOnUse = new Buff();
         }
-        public Item(string name, Sprite sprite, Stats statChange, Buff buffOnUse)
+
+        public Item(string name,string description, Sprite sprite)
         {
             this.name = name;
+            this.description = description;
+            this.sprite = sprite;
+            this.statChange = new Stats();
+            this.buffOnUse = new Buff();
+        }
+
+        public Item(string name, string description, Sprite sprite, Stats statChange, Buff buffOnUse)
+        {
+            this.name = name;
+            this.description = description;
             this.sprite = sprite;
             this.statChange = statChange;
             this.buffOnUse = buffOnUse;
