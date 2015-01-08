cbeaf2732b9e2adeb474238e3621d199cc8a79a7
Joell Stehouwer <joell@stehouwer.net>
2015-01-08 12:19:23 +0100

    Edited Item
    - Edited Item

--- Project/Programon/ProgramonEngine/Item.cs
+++ Project/Programon/ProgramonEngine/Item.cs
@@ -12,9 +12,12 @@ namespace ProgramonEngine
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public int healthToRestore;
+        public float EPToRestore;
         public Sprite Sprite { get; set; }
         public Stats StatChange { get; set; }
         public Buff BuffOnUse { get; set; }
+
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Item"/> class.
@@ -23,6 +26,8 @@ namespace ProgramonEngine
         {
             this.Name = "";
             this.Description = "";
+            healthToRestore = 0;
+            EPToRestore = 0;
             this.Sprite = new Sprite();
             this.StatChange = new Stats();
             this.BuffOnUse = new Buff();
@@ -38,6 +43,8 @@ namespace ProgramonEngine
         {
             this.Name = name;
             this.Description = description;
+            healthToRestore = 0;
+            EPToRestore = 0;
             this.Sprite = sprite;
             this.StatChange = new Stats();
             this.BuffOnUse = new Buff();
@@ -51,13 +58,28 @@ namespace ProgramonEngine
         /// <param name="sprite">The sprite of the item.</param>
         /// <param name="statChange">The stats change.</param>
         /// <param name="buffOnUse">The buff on use.</param>
-        public Item(string name, string description, Sprite sprite, Stats statChange, Buff buffOnUse)
+        public Item(string name, string description, int health, float EP, Sprite sprite, Stats statChange, Buff buffOnUse)
         {
             this.Name = name;
             this.Description = description;
+            this.healthToRestore = health;
+            this.EPToRestore = EP;
             this.Sprite = sprite;
             this.StatChange = statChange;
             this.BuffOnUse = buffOnUse;
+        }
+
+
+        public void RestoreEP(Ability ability, Item item)
+        {
+            ability.EP = Math.Min(ability.EP+(int)(ability.maxEP * item.EPToRestore / 100),ability.maxEP);
+        }
+
+        public void UseItem(Creature programon, Item item)
+        {
+            programon.programonBaseStats.health = Math.Min(programon.programonBaseStats.health + item.healthToRestore, programon.programonBaseStats.maxHealth);
+            programon.programonBaseStats += item.StatChange;
+            programon.buffList.Add(item.BuffOnUse);
         }
     }
 }
