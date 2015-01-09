4c186fd82d05032bd9c04b36a51cc4a8c7260554
Marco Havermans <marcohavermans@hotmail.nl>
2015-01-09 11:12:57 +0100

    Added inventory
    -Added inventory

---
+++ Project/Programon/Programon/Programon/Menus/Inventory.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramonEngine;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Programon
+{
+    /// <summary>
+    /// A class to create an inventory.
+    /// </summary>
+    public class Inventory : IMenu
+    {
+        private List<InventoryItem> invItem;
+        private Dictionary<Item, int> items;
+        private MainWindow mainWindow;
+        private const int ITEMWIDTH = 500;
+        private const int ITEMHEIGHT = 100;
+        private SpriteFont font;
+
+        public IGuiItem[] Childs
+        {
+            get
+            {
+                return invItem.ToArray();
+            }
+            set { }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Inventory"/> class.
+        /// </summary>
+        /// <param name="mainWindow">The main window.</param>
+        public Inventory(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        public void Initialize()
+        {
+            font = mainWindow.Content.Load<SpriteFont>("DebugFont");
+            items = new Dictionary<Item, int>();
+            invItem = new List<InventoryItem>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                AddItem(new Item("Item " + i + ".", "This is a test description of Item " + i + ". Inventory description test. This is supposed to be working.", new Sprite(mainWindow.Content.Load<Texture2D>("No"))), i * 10);
+            }
+        }
+
+        /// <summary>
+        /// Updates this instance.
+        /// </summary>
+        public void Update()
+        {
+            CalculatePositions();
+            for (int i = 0; i < invItem.Count; i++)
+            {
+                invItem[i].Update();
+            }
+        }
+
+        /// <summary>
+        /// Calculates the positions.
+        /// </summary>
+        private void CalculatePositions()
+        {
+            int row = 0;
+            int column = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if ((row * ITEMHEIGHT) + ITEMHEIGHT > mainWindow.Window.ClientBounds.Height)
+                {
+                    row = 0;
+                    column++;
+                }
+                int xPos = column * ITEMWIDTH;
+                int yPos = row * ITEMHEIGHT;
+                invItem[i].SetPosition(new Vector2(xPos, yPos));
+                invItem[i].SetSize(new Vector2(ITEMWIDTH, ITEMHEIGHT));
+                row++;
+            }
+        }
+
+        /// <summary>
+        /// Adds an item.
+        /// If the amount is equal or lower than 0 the item won't be added.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="amount">The amount.</param>
+        public void AddItem(Item item, int amount)
+        {
+            if (amount > 0)
+            {
+                if (items.ContainsKey(item))
+                {
+                    items[item] = amount;
+
+                }
+                else
+                {
+                    items.Add(item, amount);
+                    invItem.Add(new InventoryItem(new Rectangle(0, 0, ITEMWIDTH, ITEMHEIGHT), item, amount, font));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the amount of an item.
+        /// If the amount is lower or equal than 0 the item gets removed.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="amount">The amount.</param>
+        public void SetAmount(Item item, int amount)
+        {
+            if (items.ContainsKey(item))
+            {
+                if (amount > 0)
+                {
+                    items[item] = amount;
+                    foreach (InventoryItem inv in invItem)
+                    {
+                        if (inv.Item == item)
+                        {
+                            inv.SetAmount(amount);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    RemoveItem(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the item. If the inventory contains it.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void RemoveItem(Item item)
+        {
+            if (items.ContainsKey(item))
+            {
+                items.Remove(item);
+                foreach (InventoryItem inv in invItem)
+                {
+                    if (inv.Item == item)
+                    {
+                        invItem.Remove(inv);
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches the item with a specified name. Throws an <see cref="KeyNotFoundException"/> when not found.
+        /// </summary>
+        /// <param name="name">The name of the item to search.</param>
+        /// <returns>A KeyValuePair with the Item and the amount./returns>
+        /// <exception cref="KeyNotFoundException">The searched name doesn't exists in the inventory.</exception>
+        public KeyValuePair<Item, int> GetItem(string name)
+        {
+            KeyValuePair<Item, int> foundItem = new KeyValuePair<Item, int>();
+            foreach (KeyValuePair<Item, int> item in items)
+            {
+                if (item.Key.Name == name)
+                {
+                    foundItem = item;
+                    break;
+                }
+            }
+
+            if (foundItem.Equals(default(KeyValuePair<Item, int>)))
+            {
+                throw new KeyNotFoundException("The searched name doesn't exists in the inventory.");
+            }
+
+            return foundItem;
+        }
+
+        /// <summary>
+        /// Gets the items.
+        /// </summary>
+        /// <returns>All the items in the inventory.</returns>
+        public Dictionary<Item, int> GetItems()
+        {
+            return this.items;
+        }
+
+        /// <summary>
+        /// Gets the amount of the item in the current inventory. Returns 0 if the item doesn't exists.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public int GetItemAmount(Item item)
+        {
+            if (items.ContainsKey(item))
+            {
+                return items[item];
+            }
+
+            return 0;
+        }
+    }
+}
--- Project/Programon/ProgramonEngine/GUI Items/InventoryItem.cs
+++ Project/Programon/ProgramonEngine/GUI Items/InventoryItem.cs
@@ -14,19 +14,29 @@ namespace ProgramonEngine
     {
         private Rectangle imageRectangle;
         private Rectangle itemRectangle;
-        private Item item;
+        public Item Item { get; private set; }
+        private int amount;
         private SpriteFont textFont;
-        private Vector2 itemNameScale;
-        private Vector2 itemDescriptionScale;
         private Vector2 itemNamePosition;
+        private Vector2 itemNameScale;
         private Vector2 itemDescPosition;
+        private Vector2 itemDescScale;
+        private Vector2 itemAmountPosition;
+        private Vector2 itemAmountScale;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="InventoryItem"/> class.
         /// </summary>
         public InventoryItem()
         {
-            this.item = new Item();
+            this.Item = new Item();
+            this.itemNamePosition = new Vector2();
+            this.itemNameScale = new Vector2();
+            this.itemDescPosition = new Vector2();
+            this.itemDescScale = new Vector2();
+            this.itemAmountPosition = new Vector2();
+            this.itemAmountScale = new Vector2();
+            this.amount = 0;
             this.itemRectangle = new Rectangle();
             this.imageRectangle = new Rectangle();
             this.textFont = null;
@@ -38,13 +48,14 @@ namespace ProgramonEngine
         /// <param name="ItemRectangle">The item rectangle. (Position and Size)</param>
         /// <param name="item">The item in the inventory.</param>
         /// <param name="font">The font to display text with.</param>
-        public InventoryItem(Rectangle ItemRectangle, Item item, SpriteFont font)
+        public InventoryItem(Rectangle ItemRectangle, Item item, int amount, SpriteFont font)
         {
-            this.item = item;
+            this.Item = item;
+            this.amount = amount;
             this.itemRectangle = ItemRectangle;
             this.imageRectangle = new Rectangle();
             this.textFont = font;
-            this.item.Description = StringExtensions.WrapText(item.Description, (itemRectangle.Width - imageRectangle.Width), textFont);
+            this.Item.Description = StringExtensions.WrapText(item.Description, (itemRectangle.Width - imageRectangle.Width), textFont);
         }
 
         /// <summary>
@@ -54,7 +65,7 @@ namespace ProgramonEngine
         {
             imageRectangle = new Rectangle(itemRectangle.X, itemRectangle.Y, itemRectangle.Height, itemRectangle.Height);
 
-            Vector2 size = textFont.MeasureString(item.Name);
+            Vector2 size = textFont.MeasureString(Item.Name);
 
             float itemWidth = size.X;
             float itemHeight = size.Y;
@@ -65,7 +76,7 @@ namespace ProgramonEngine
                 FontScaleItemW = 1f;
             }
 
-            float FontScaleItemH = (itemRectangle.Height / 3) /itemHeight;
+            float FontScaleItemH = (itemRectangle.Height / 3) / itemHeight;
 
             itemWidth = itemWidth * FontScaleItemW;
             itemHeight = itemHeight * FontScaleItemH;
@@ -74,26 +85,69 @@ namespace ProgramonEngine
 
             itemNamePosition = new Vector2(itemRectangle.X + imageRectangle.Width, itemRectangle.Y);
             itemDescPosition = new Vector2(itemRectangle.X + imageRectangle.Width, itemRectangle.Y + itemHeight);
-            
-            Vector2 descriptionSize = textFont.MeasureString(item.Description);
+
+            Vector2 descriptionSize = textFont.MeasureString(Item.Description);
 
             float descriptionWidth = descriptionSize.X;
             float descriptionHeight = descriptionSize.Y;
             float FontScaleDescW = (itemRectangle.Width - imageRectangle.Width) / descriptionWidth;
-            float FontScaleDescH = (itemRectangle.Height - itemHeight)/ descriptionHeight;
+            float FontScaleDescH = (itemRectangle.Height - itemHeight) / descriptionHeight;
 
             descriptionWidth = descriptionWidth * FontScaleDescW;
             descriptionHeight = descriptionHeight * FontScaleDescH;
 
-            itemDescriptionScale = new Vector2(FontScaleDescW, FontScaleDescH);
+            itemDescScale = new Vector2(FontScaleDescW, FontScaleDescH);
+
+            Vector2 amountSize = textFont.MeasureString("x" + amount);
+            float amountWidth = amountSize.X;
+            float amountHeight = amountSize.Y;
+
+            float FontScaleAmountW = (imageRectangle.Width / 2) / amountWidth;
+            float FontScaleAmountH = (imageRectangle.Height / 6) / amountHeight;
+
+            amountWidth = amountWidth * FontScaleAmountW;
+            amountHeight = amountHeight * FontScaleAmountH;
+
+            itemAmountScale = new Vector2(FontScaleAmountW, FontScaleAmountH);
+            itemAmountPosition = new Vector2((imageRectangle.X + imageRectangle.Height) - amountWidth, (imageRectangle.Y + imageRectangle.Height) - amountHeight);
         }
 
         public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(Sprite.FromStaticColor(Color.Black, Color.White, spriteBatch.GraphicsDevice).Texture, itemRectangle, Color.White);
-            spriteBatch.Draw(item.Sprite.Texture, imageRectangle, Color.White);
-            spriteBatch.DrawString(textFont, item.Name, itemNamePosition, Color.White, 0f,new Vector2(0,0), itemNameScale, SpriteEffects.None, 0);
-            spriteBatch.DrawString(textFont, item.Description, itemDescPosition, Color.White, 0f,new Vector2(0,0),itemDescriptionScale,SpriteEffects.None, 0);
+            spriteBatch.Draw(Sprite.FromStaticColor(Color.Yellow, Color.White, spriteBatch.GraphicsDevice).Texture, imageRectangle, Color.White);
+            spriteBatch.DrawString(textFont, "x" + amount, itemAmountPosition, Color.Red, 0f, new Vector2(0, 0), itemAmountScale, SpriteEffects.None, 0);
+            spriteBatch.DrawString(textFont, Item.Name, itemNamePosition, Color.White, 0f, new Vector2(0, 0), itemNameScale, SpriteEffects.None, 0);
+            spriteBatch.DrawString(textFont, Item.Description, itemDescPosition, Color.White, 0f, new Vector2(0, 0), itemDescScale, SpriteEffects.None, 0);
+        }
+
+        /// <summary>
+        /// Sets the amount of the item.
+        /// </summary>
+        /// <param name="amount">The amount.</param>
+        public void SetAmount(int amount)
+        {
+            this.amount = amount;
+        }
+
+        /// <summary>
+        /// Sets the position.
+        /// </summary>
+        /// <param name="position">The new position.</param>
+        public void SetPosition(Vector2 position)
+        {
+            this.itemRectangle.X = (int) position.X;
+            this.itemRectangle.Y = (int) position.Y;
+        }
+
+        /// <summary>
+        /// Sets the size.
+        /// </summary>
+        /// <param name="size">The new size.</param>
+        public void SetSize(Vector2 size)
+        {
+            this.itemRectangle.Width = (int) size.X;
+            this.itemRectangle.Height = (int) size.Y;
         }
     }
 }
